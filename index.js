d4db792d9e25c2c6d3cf7f86a04ae549189dfff1
seireitou <ausis@mail.com>
2021-10-26 00:52:23 -0500

    adds discord token to config file, allows discord bot to be logged in

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
+const config = require('./config.json')
 
-const client = new Discord.Client();
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+
+
+
+
+client.once('ready', () => {
+    console.log('Arete is online!');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(config.token)
