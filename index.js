0d598fe678d4792c49f97104fbbf7e692ea08dca
seireitou <ausis@mail.com>
2021-10-26 19:28:23 -0500

    exports command files into index.js so they can be set to client

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const { Client, Intents, Interaction } = require('discord.js');
-const { token } = require('./config.json')
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
 
 
 client.once('ready', () => {
